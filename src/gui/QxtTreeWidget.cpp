cdcfbbbb0db2f7ee3990b006ea5f3c532fc78028
jpnurmi <devnull@localhost>
2007-02-02 20:38:28 +0000

    Rewrote QxtTreeWidget.

--- src/gui/QxtTreeWidget.cpp
+++ src/gui/QxtTreeWidget.cpp
@@ -1,135 +1,165 @@
 /*******************************************************************
 Qt extended Library 
-Copyright (C) 2006 Adam Higerd and Arvid Picciani
+Copyright (C) 2007 J-P Nurmi <jpnurmi@gmail.com>
 released under the Terms of LGPL (see the LICENSE file)
 *******************************************************************/
 
 #include "QxtTreeWidget.h"
-#include <QPainter>
-#include <QDebug>
-#include <QStyle>
+#include "QxtItemDelegate.h"
 #include <QHeaderView>
-#include <QMouseEvent>
-#include <QMimeData>
-#include <QApplication>
-#include <QByteArray>
-#include <QDebug>
 
-
-
-QxtTreeWidget::QxtTreeWidget(QWidget * parent) :QTreeWidget(parent)
+class QxtTreeWidgetPrivate : public QObject, public QxtPrivate<QxtTreeWidget>
 {
-	dragstartitem=0;
-}
-
-
-
-
-
-
-void QxtTreeWidget::drawRow ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index )const
-{
-    if (index.parent() == QModelIndex()) {
-        QStyleOptionButton button;
-
-        button.rect = option.rect;
-        button.rect.setWidth(header()->width());
-        if(isExpanded(index)) button.state = QStyle::State_Sunken; else button.state = QStyle::State_Raised;
-        style()->drawControl(QStyle::CE_PushButton, &button, painter, this);
-
-        button.rect.setX(indentation());
-        button.rect.setWidth(width() - indentation());
-        painter->drawText(button.rect, Qt::AlignLeft | Qt::AlignVCenter | Qt::TextSingleLine,
-                model()->data(index, Qt::DisplayRole).toString());
-
-        button.rect.setX(option.rect.left());
-        button.rect.setWidth(indentation());
-        drawBranches(painter, button.rect, index);
-    } else {
-        QTreeWidget::drawRow(painter, option, index);
-    }
-}
-
-
-
-
-
-void QxtTreeWidget::mousePressEvent(QMouseEvent *event)
-{
-
-	QTreeWidgetItem *i = itemAt(event->pos());
-
-	if (i)
-		{
-		if ( i->parent())
-			{
-			dragstartitem = i;
-			QTreeWidget::mousePressEvent(event);
-
-			}
-		else
-			{
-			dragstartitem =0;
-			
-    			if(isItemExpanded(i))
-        			collapseItem(i);
-    			else
-        			expandItem(i);
-
-			}
-		}
-	else
-		{
-		dragstartitem =0;
-		QTreeWidget::mousePressEvent(event);
-		}
-
-	if (event->button() == Qt::LeftButton)
-        	dragStartPosition = event->pos();
-
+	Q_OBJECT
+public:
+	QXT_DECLARE_PUBLIC(QxtTreeWidget);
+	QxtItemDelegate* delegate() const;
 	
-}
+private slots:
+	void informStartEditing(const QModelIndex& index);
+	void informFinishEditing(const QModelIndex& index);
+	void expandCollapse(QTreeWidgetItem* item);
+};
 
-
-
-void QxtTreeWidget::mouseMoveEvent(QMouseEvent *event)
+QxtItemDelegate* QxtTreeWidgetPrivate::delegate() const
 {
+	return dynamic_cast<QxtItemDelegate*>(qxt_p().itemDelegate());
+}
 
-if (event->button() == Qt::LeftButton)
+
+void QxtTreeWidgetPrivate::informStartEditing(const QModelIndex& index)
 {
-
-    if (!dragstartitem)
-        return;
-    if ((event->pos() - dragStartPosition).manhattanLength()
-         < QApplication::startDragDistance())
-        return;
-
-	startDrag(model()->supportedDragActions());
-	dragstartitem =0;
-
+	QTreeWidgetItem* item = qxt_p().itemFromIndex(index);
+	Q_ASSERT(item);
+	qxt_p().itemEditingStarted(item);
 }
 
-
-QTreeWidgetItem *i = itemAt(event->pos());
-
-  if ((i) && ( i->parent())) 
-  	QTreeWidget::mouseMoveEvent(event);
-
-
-}
-
-void QxtTreeWidget::mouseReleaseEvent(QMouseEvent *event)
-
+void QxtTreeWidgetPrivate::informFinishEditing(const QModelIndex& index)
 {
-/*QTreeWidgetItem *i = itemAt(event->pos());
- if ((i) && (! i->parent())) return;*/
-QTreeWidget::mouseReleaseEvent(event);
+	QTreeWidgetItem* item = qxt_p().itemFromIndex(index);
+	Q_ASSERT(item);
+	qxt_p().itemEditingFinished(item);
 }
 
-
-QTreeWidgetItem * QxtTreeWidget::ItemDragStarted()
+void QxtTreeWidgetPrivate::expandCollapse(QTreeWidgetItem* item)
 {
-return dragstartitem;
+	if (item && !item->parent())
+		qxt_p().setItemExpanded(item, !qxt_p().isItemExpanded(item));
 }
 
+#include "QxtTreeWidget.moc"
 
+/*!
+    \class QxtTreeWidget QxtTreeWidget
+    \ingroup gui
+    \brief An extended tree widget.
+
+    QxtTreeWidget blaablaa.
+
+    \note Requires Qt 4.2 or newer.
+ */
+
+/*!
+    \fn QxtTreeWidget::itemEditingStarted(QTreeWidgetItem* item)
+
+    This signal is emitted immediately after the editing of \a item has been started.
+
+    \sa itemEditingFinished()
+ */
+
+/*!
+    \fn QxtTreeWidget::itemEditingFinished(QTreeWidgetItem* item)
+
+    This signal is emitted immediately after the editing of \a item has been finished.
+
+    \sa itemEditingStarted()
+ */
+
+/*!
+    Constructs a new QxtTreeWidget with \a parent.
+ */
+QxtTreeWidget::QxtTreeWidget(QWidget* parent) : QTreeWidget(parent)
+{
+	QXT_INIT_PRIVATE(QxtTreeWidget);
+	setItemDelegate(new QxtItemDelegate(this));
+	setRootIsDecorated(false);
+	setColumnCount(1);
+	header()->hide();
+	connect(this, SIGNAL(itemPressed(QTreeWidgetItem*, int)),
+		&qxt_d(), SLOT(expandCollapse(QTreeWidgetItem*)));
+}
+
+/*!
+    Destructs the tree widget.
+ */
+QxtTreeWidget::~QxtTreeWidget()
+{
+}
+
+/*!
+    \property QxtTreeWidget::rootDecorated
+    \brief This property holds whether top level items are decorated
+
+    Top level items are decorated if this property is \b true.
+    The default value is \b true.
+
+    \sa decorationStyle elideMode 
+ */
+bool QxtTreeWidget::isRootDecorated() const
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->isRootDecorated();
+	return true;
+}
+
+void QxtTreeWidget::setRootDecorated(bool decorate)
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->setRootDecorated(decorate);
+}
+
+/*!
+    \property QxtItemDelegate::decorationStyle
+    \brief This property holds the top level item decoration style
+
+    Top level items are decorated according to this property.
+    The default value is \b QxtItemDelegate::Buttonlike.
+
+    \note The property has effect only in case rootDecorated is \b true.
+
+    \sa rootDecorated QxtItemDelegate::DecorationStyle
+ */
+QxtItemDelegate::DecorationStyle QxtTreeWidget::decorationStyle() const
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->decorationStyle();
+}
+
+void QxtTreeWidget::setDecorationStyle(QxtItemDelegate::DecorationStyle style)
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->setDecorationStyle(style);
+}
+
+/*!
+    \property QxtTreeWidget::elideMode
+    \brief This property holds the text elide mode
+
+    The text of a decorated top level item is elided according to this property.
+    The default value is \b Qt::ElideMiddle.
+
+    \note The property has effect only in case rootDecorated is \b true.
+
+    \sa rootDecorated
+ */
+Qt::TextElideMode QxtTreeWidget::elideMode() const
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->elideMode();
+}
+
+void QxtTreeWidget::setElideMode(Qt::TextElideMode mode)
+{
+	Q_ASSERT(qxt_d().delegate());
+	return qxt_d().delegate()->setElideMode(mode);
+}
--- src/gui/QxtTreeWidget.h
+++ src/gui/QxtTreeWidget.h
@@ -1,60 +1,43 @@
 /*******************************************************************
 Qt extended Library 
-Copyright (C) 2006 Adam Higerd and Arvid Picciani
+Copyright (C) 2007 J-P Nurmi <jpnurmi@gmail.com>
 released under the Terms of LGPL (see the LICENSE file)
 *******************************************************************/
-
-/**
-\class QxtTreeWidget QxtTreeWidget
-
-\ingroup gui
-
-\brief QTreeWidget with a customized drawing for parent items
-
-
-
-pretty top items, to make them more visible
-
-hint: set the top level items SizeHint to something more big to make it look good
-
-
-to implement your own drag, subclas void startDrag(Qt::DropActions );  and use QTreeWidgetItem * ItemDragStarted();  to get the item the drag started from
-*/
 
 #ifndef QXTTREEWIDGET_H
 #define QXTTREEWIDGET_H
 
-
 #include <QTreeWidget>
-#include <QPoint>
-#include <QTreeWidgetItem>
-#include "QxtDefines.h"
+#include <QxtDefines.h>
+#include <core/QxtPimpl.h>
+#include <QxtItemDelegate.h>
+
+class QxtTreeWidgetPrivate;
 
 class QXT_DLLEXPORT QxtTreeWidget : public QTreeWidget
-	{
-	
+{
 	Q_OBJECT
+	QXT_DECLARE_PRIVATE(QxtTreeWidget);
+	Q_PROPERTY(bool rootDecorated READ isRootDecorated WRITE setRootDecorated)
+	Q_PROPERTY(QxtItemDelegate::DecorationStyle decorationStyle READ decorationStyle WRITE setDecorationStyle)
+	Q_PROPERTY(Qt::TextElideMode elideMode READ elideMode WRITE setElideMode)
 	
-	public:
-		QxtTreeWidget(QWidget *parent= 0);
-		
-		///returns a pointet to the QTreeWidgetItem the last drag started from. be warned: check if it is valid
-		QTreeWidgetItem * ItemDragStarted();
-		void resetDrag(){dragstartitem=0;}
+public:
+	explicit QxtTreeWidget(QWidget* parent = 0);
+	virtual ~QxtTreeWidget();
+	
+	bool isRootDecorated() const;
+	void setRootDecorated(bool decorate);
+	
+	QxtItemDelegate::DecorationStyle decorationStyle() const;
+	void setDecorationStyle(QxtItemDelegate::DecorationStyle style);
+	
+	Qt::TextElideMode elideMode() const;
+	void setElideMode(Qt::TextElideMode mode);
+	
+signals:
+	void itemEditingStarted(QTreeWidgetItem* item);
+	void itemEditingFinished(QTreeWidgetItem* item);
+};
 
-	protected:
-
-		void drawRow ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
- 		void mousePressEvent(QMouseEvent *event);
- 		void mouseMoveEvent(QMouseEvent *event);
-		void mouseReleaseEvent(QMouseEvent *event);
-
-
-	private:
-		
-		QPoint  dragStartPosition;
-		QTreeWidgetItem * dragstartitem;
-	};
-
-
-#endif
+#endif // QXTTREEWIDGET_H
