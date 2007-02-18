ffc994d6e211f57ddf1a231c3c9a13a383986108
aep <none@none>
2007-02-18 12:46:24 +0000

    docs and a division by zero
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40152

--- src/media/QxtAVFile.cpp
+++ src/media/QxtAVFile.cpp
@@ -3,7 +3,22 @@
 
 
 
+/**
+\class QxtAVFile QxtAVFile
 
+\ingroup media
+
+\brief AV Decoder
+
+A very easy to use interface for libavcodec and libavformat from ffmpeg.
+
+*/
+
+
+
+/*!
+    Constructs a new QxtAVFile with \a parent.
+ */
  QxtAVFile::QxtAVFile(QObject *parent):QObject(parent)
 	{
 	}
@@ -14,43 +29,66 @@ QxtAVFile::~QxtAVFile()
 	qxt_d().destroy();
 	}
 //-------------------------------------------------------------
-
+/*!
+   opens a stream or file from  \a url.
+ */
 QxtError QxtAVFile::open(QString url)
 	{
 	return qxt_d().open(url);
 	}
 //-------------------------------------------------------------
+/*!
+returns the preload state
+
+preloading is not implemented yet
+ */
 
 bool QxtAVFile::preloaded() const
 	{
 	return false;
 	}
 //-------------------------------------------------------------
+/*!
+sets the preload state
+
+preloading is not implemented yet
+ */
 
 QxtError QxtAVFile::setPreloaded(const bool)
 	{
 	QXT_DROP_OK
 	}
 //-------------------------------------------------------------
+/*!
+read an amount of \a length  shorts into the target buffer.
+*/
 
 QxtError QxtAVFile::read(short* target, unsigned long length)
 	{
 	return qxt_d().read(target,length);	
 	}
 //-------------------------------------------------------------
+/*!
+read an amount of \a length  floats into the target buffer.
+*/
 
 QxtError QxtAVFile::read(float* target, unsigned long length)
 	{
 	return qxt_d().read(target,length);	
 	}
 //-------------------------------------------------------------
+/*!
+read an amount of \a length  chars into the target buffer.
+*/
 
 QxtError QxtAVFile::read(char * target, unsigned long length)
 	{
 	return qxt_d().read(target,length);	
 	}
 //-------------------------------------------------------------
-
+/*!
+  returns true when the end of the input stream has been reached
+ */
 bool 	QxtAVFile::isEof()
 	{
 	return qxt_d().isEof();	
@@ -58,21 +96,32 @@ bool 	QxtAVFile::isEof()
 
 
 //-------------------------------------------------------------
+/*!
+  the length of the url in seconds.
 
+that will return 0.0 if the input is a network stream
+ */
 double QxtAVFile::length() const
 	{
 	return qxt_d().length();
 	}
 
 //-------------------------------------------------------------
+/*!
+   seek to   \a time in seconds.
 
+
+that won't do anything for network  streams.
+ */
 QxtError QxtAVFile::seek(double time)
 	{
 	return qxt_d().seek(time);
 	}
 
 //-------------------------------------------------------------
-
+/*!
+current  progress in seconds.
+ */
 double QxtAVFile::time() const
 	{
 	return qxt_d().time();
@@ -80,6 +129,9 @@ double QxtAVFile::time() const
 
 //-------------------------------------------------------------
 
+/*!
+seek to   \a time in percentage.
+*/
 
 QxtError QxtAVFile::seekP(char time)
 	{
@@ -88,6 +140,12 @@ QxtError QxtAVFile::seekP(char time)
 	}
 
 //-------------------------------------------------------------
+/*!
+current progress in percentage.
+
+returns 0 for a stream
+*/
+
 char 	QxtAVFile::timeP()   const
 	{
 	return qxt_d().timeP();
@@ -95,7 +153,10 @@ char 	QxtAVFile::timeP()   const
 
 
 //-------------------------------------------------------------
-	
+
+/*!
+samplerate of the file
+*/	
 unsigned long QxtAVFile::samplerate()
 	{
 	return qxt_d().samplerate();
