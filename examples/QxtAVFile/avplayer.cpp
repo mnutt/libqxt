51748d491dc002309bee98093b226fb14b5eb265
aep <none@none>
2007-04-04 10:11:31 +0000

    adding license to tests and examples, in case someone wants to copy them (may be needed, maybe not, who knows)
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40321

--- examples/QxtAVFile/avplayer.cpp
+++ examples/QxtAVFile/avplayer.cpp
@@ -1,15 +1,24 @@
-/*!
-SDL + QxtAVFile example.
-(c) 2007 Arvid Picciani (aep@exysREMOVETHIS.org) GPL
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/****************************************************************************
+**
+** Copyright (C) Qxt Foundation. Some rights reserved.
+**
+** This file is part of the QxtCore module of the Qt eXTension library
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or any later version.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** There is aditional information in the LICENSE file of libqxt.
+** If you did not receive a copy of the file try to download it or
+** contact the libqxt Management
+** 
+** <http://libqxt.sourceforge.net>  <aep@exys.org>
+**
+****************************************************************************/
 #include <SDL/SDL.h>
 #include <QCoreApplication>
 #include <QxtAVFile>
