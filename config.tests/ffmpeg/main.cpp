ed4934c919f91b70c35230402a84d86207d7eb1f
aep <none@none>
2007-04-14 16:29:14 +0000

    ffmpeg version check
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40373

--- config.tests/ffmpeg/main.cpp
+++ config.tests/ffmpeg/main.cpp
@@ -2,6 +2,9 @@
 #ifndef INT64_C
         #define INT64_C Q_INT64_C
 #endif
+
+
+
 #include <ffmpeg/avcodec.h>
 #include <ffmpeg/avformat.h>
 
@@ -11,6 +14,12 @@ int main (int,char**)
         av_register_all();
         avcodec_init();
         avcodec_register_all();
+
+	if(LIBAVCODEC_VERSION_INT>>16!=51)
+		{
+		qDebug("incompatible major version %i",LIBAVCODEC_VERSION_INT>>16);
+		return 3;
+		}
 	return 0;
 	}
 
