e7f8159ec5897ad78dc50020da665665d0f9747d
aep <devnull@localhost>
2007-04-20 15:24:31 +0000

    tooltip attemp

--- src/gui/QxtHeaderView.h
+++ src/gui/QxtHeaderView.h
@@ -15,15 +15,16 @@ class QXT_GUI_EXPORT QxtHeaderView : public QHeaderView
 		QxtHeaderView (Qt::Orientation orientation ,QWidget * parent);	
 		void addAction(QAction * action);
 	protected:
-		virtual int subPaint(QPainter * painter, const QRect & rect, int logicalIndex,QSize  icon_size, int spacing) const;
-		virtual int subClick(QMouseEvent * m,QSize icon_size, int spacing ) ;
-
+		virtual void subPaint(QPainter * painter, const QRect & rect, int logicalIndex,QSize  icon_size, int spacing) const;
+		virtual void subClick(QMouseEvent * m,QSize icon_size, int spacing ) ;
+                virtual int  subWidth(QSize icon_size, int spacing) const;
 	signals:
 		void checkBoxChanged(bool);
 
         private:
 		virtual	void paintSection ( QPainter * painter, const QRect & rect, int logicalIndex ) const;
 		virtual void mousePressEvent ( QMouseEvent * m );
+                virtual void mouseMoveEvent ( QMouseEvent * event );
 
                 QxtHeaderViewPrivate * priv;
 
