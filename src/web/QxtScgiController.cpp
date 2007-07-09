ed59ca8ac931f21943a5afd0243a1dceb544c42e
aep <none@none>
2007-07-09 21:13:43 +0000

    removed the weird hack that allowed calling a random not-matching slot
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40577

--- src/web/QxtScgiController.cpp
+++ src/web/QxtScgiController.cpp
@@ -49,195 +49,92 @@ int QxtScgiController::invoke(QTcpSocket * socket,server_t & SERVER_i)
         QTextStream strm (socket);
         stream_m=  &strm;
 
-
-
-
-        /**
-        since the meta object passes the method information all wrong we need to try and error
-        if int index(foo,foo,foo) doesnt exist, we try index(foo,foo,foo,"") and so on
-        if that doesnt work it tryes foo,foo and foo
-        **/
-        ///TODO  so ugly :/
-
         int retVal=666;
-        bool up=true;
+	if (args.count()>8)
+		{
+		if (!QMetaObject::invokeMethod(this, action,Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6)),Q_ARG(QString, args.at(8))))
+			{
+			retVal=4042;
+			}
+		}
+	else if (args.count()>7)
+		{
+		if (!QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6)),Q_ARG(QString, args.at(7))
+		))
+			{
+			retVal=4042;
+			}
 
-
-        bool tryed[9]={false,false,false,false,false,false,false,false,false};
-
-        forever
-                {
-                qDebug()<<"invoke"<<action<<args.count();
-                if (args.count()>8)
-                        {
-                        if (tryed[9] || !QMetaObject::invokeMethod(this, action,Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6)),Q_ARG(QString, args.at(8))))
-                                {
-                                tryed[9]=true;
-                                up=false;
-                                args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        }
-                else if (args.count()>7)
-                        {
-                        if (tryed[8] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6)),Q_ARG(QString, args.at(7))
-                        ))
-                                {
-                                tryed[8]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        }
-                else if (args.count()>6)
-                        {
-                        if (tryed[7] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6))
-                        ))
-                                {
-                                tryed[7]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        }
-                else if (args.count()>5)
-                        {
-                        if (tryed[6] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5))
-                        ))
-                                {
-                                tryed[6]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else if ( args.count()>4)
-                        {
-                        if (tryed[5] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ,Q_ARG(QString, args.at(4))
-                        ))
-                                {
-                                tryed[5]=true;
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else if (args.count()>3)
-                        {
-                        if (tryed[4] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
-                        ))
-                                {
-                                tryed[4]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else if (args.count()>2)
-                        {
-                        if (tryed[3] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2))
-                        ))
-
-                                {
-                                tryed[3]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else if (args.count()>1)
-                        {
-                        if (tryed[2] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
-                        Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1))
-                        ))
-
-                                {
-                                tryed[2]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else if (args.count()>0)
-                        {
-                        if (tryed[1] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,Q_ARG(QString, args.at(0))))
-
-                                {
-                                tryed[1]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        args.removeLast();
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                else
-                        {
-                        if (tryed[0] || !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ))
-                                {
-                                tryed[0]=true;
-
-                                if(up)
-                                        args<<QByteArray();
-                                else
-                                        return 4042; ///TODO X-REDIRECT
-                                continue;
-                                }
-                        else 
-                                break;
-                        } 
-                }
+		}
+	else if (args.count()>6)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5)),Q_ARG(QString, args.at(6))
+		))
+			{
+			retVal=4042;
+			}
+		}
+	else if (args.count()>5)
+		{
+		if (!QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		,Q_ARG(QString, args.at(4)),Q_ARG(QString, args.at(5))
+		))
+			{
+			retVal=4042;
+			}
+		} 
+	else if ( args.count()>4)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		,Q_ARG(QString, args.at(4))
+		))
+			{
+			retVal=4042;
+			}
+		} 
+	else if (args.count()>3)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2)),Q_ARG(QString, args.at(3))
+		))
+			{
+			retVal=4042;
+			}
+		} 
+	else if (args.count()>2)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1)),Q_ARG(QString, args.at(2))
+		))
+			retVal=4042;
+		} 
+	else if (args.count()>1)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,
+		Q_ARG(QString, args.at(0)),Q_ARG(QString, args.at(1))
+		))
+			retVal=4042;
+		} 
+	else if (args.count()>0)
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ,Q_ARG(QString, args.at(0))))
+			retVal=4042;
+		} 
+	else
+		{
+		if ( !QMetaObject::invokeMethod(this, (action),Q_RETURN_ARG(int, retVal) ))
+			{
+			retVal=4042;
+			}
+		}
 
 	stream_m->flush ();
         stream_m=0;
