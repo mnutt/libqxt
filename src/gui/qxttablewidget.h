8c3f12da2fadc917c19b2b077ebaf5dae1704998
jpnurmi <none@none>
2007-07-18 18:43:22 +0000

    CPL header comments to gui ~nb
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40611

--- src/gui/qxttablewidget.h
+++ src/gui/qxttablewidget.h
@@ -1,17 +1,24 @@
 /****************************************************************************
 **
-** Copyright (C) J-P Nurmi <jpnurmi@gmail.com>. Some rights reserved.
+** Copyright (C) Qxt Foundation. Some rights reserved.
 **
-** This file is part of the QxtGui module of the
-** Qt eXTension library <http://libqxt.sourceforge.net>
+** This file is part of the QxtCore module of the Qt eXTension library
 **
-** This library is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public
-** License as published by the Free Software Foundation; either
-** version 2.1 of the License, or any later version.
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of th Common Public License, version 1.0, as published by
+** IBM.
 **
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+** This file is provided "AS IS", without WARRANTIES OR CONDITIONS OF ANY
+** KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY
+** WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR
+** FITNESS FOR A PARTICULAR PURPOSE. 
+**
+** You should have received a copy of the CPL along with this file.
+** See the LICENSE file and the cpl1.0.txt file included with the source
+** distribution for more information. If you did not receive a copy of the
+** license, contact the Qxt Foundation.
+** 
+** <http://libqxt.sourceforge.net>  <libqxt@gmail.com>
 **
 ****************************************************************************/
 #ifndef QXTTABLEWIDGET_H
