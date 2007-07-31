9a119ef53271fa04070b7d9d820f68fb86590949
aep <none@none>
2007-07-31 17:58:25 +0000

    QxtStdin-> QxtStdio
    --HG--
    rename : src/core/qxtstdin.cpp => src/core/qxtstdio.cpp
    rename : src/core/qxtstdin.h => src/core/qxtstdio.h
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40665

---
+++ src/core/qxtstdio.cpp
@@ -0,0 +1,68 @@
+#include "qxtstdio.h" 
+#include <cstdio>
+#include <QSocketNotifier>
+
+/**
+\class QxtStdio QxtStdio
+
+\ingroup core
+
+\brief QIODevice support for stdin and stdout
+
+including readyRead() signal
+note that when using this class, the buffer for stdin will be disabled, and NOT reenabled on destruction
+
+perfect as a counter part for QProcess
+*/
+
+QxtStdio::QxtStdio(QObject * parent):QIODevice(parent)
+	{
+	setvbuf ( stdin , NULL , _IONBF , 0 );
+	setOpenMode (QIODevice::ReadWrite);
+	notify = new QSocketNotifier ( fileno(stdin),QSocketNotifier::Read,this );
+	connect(notify, SIGNAL(activated(int)),this,SLOT(activated(int)));
+	}
+
+qint64 QxtStdio::readData ( char * data, qint64 maxSize )
+	{
+	qint64 i=0;
+	for (;i<maxSize;i++)
+		{
+		if (inbuffer.isEmpty())
+			break;
+		(*data++)=inbuffer.dequeue();
+		}
+	return i;
+	}
+qint64 QxtStdio::writeData ( const char * data, qint64 maxSize )
+	{
+	qint64 i=0;
+	for (;i<maxSize;i++)
+		{
+		char c=*data++;
+		if (putchar(c)!=c)
+			break;
+		}
+// 	emit(bytesWritten (i)); ///FIXME: acording to the docs this may not be recoursive. how do i prevent that?
+	return i;
+	}
+
+
+bool QxtStdio::isSequential () const
+	{
+	return true;
+	}
+
+
+qint64 QxtStdio::bytesAvailable () const
+	{
+	return inbuffer.count();
+	}
+
+void QxtStdio::activated(int )	
+	{
+	inbuffer.enqueue(getchar());
+ 	emit(readyRead ());
+	}
+
+
