cfaad10ec46c6b3d1e103ce93e098e1b4aedeea3
aep <none@none>
2007-10-20 15:42:00 +0000

    fully working QxtPipe
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%40899

--- tests/pipe/main.cpp
+++ tests/pipe/main.cpp
@@ -8,7 +8,7 @@ class QxtPipeTest: public QObject
 {
 Q_OBJECT 
 private slots:
-    void readwrite()
+    void simple()
     {
         QxtPipe p1;
         QxtPipe p2;
@@ -16,15 +16,57 @@ private slots:
         p1.write("hi");
         QVERIFY(p2.readAll()=="hi");
     }
+    void chain()
+    {
+        QxtPipe p1;
+        QxtPipe p2;
+        QxtPipe p3;
+        p1|p2;
+        p2|p3;
+        p1.write("hi");
+        QVERIFY(p3.readAll()=="hi");
+    }
     void bidirectional()
     {
         QxtPipe p1;
         QxtPipe p2;
         p1|p2;
-        p2|p1;
+
         p1.write("hi");
         QVERIFY(p2.readAll()=="hi");
-        QVERIFY(p2.readAll()==QString());
+        QVERIFY(p1.bytesAvailable()==0);
+
+        p2.write("rehi");
+        QVERIFY(p1.readAll()=="rehi");
+        QVERIFY(p2.bytesAvailable()==0);
+    }
+    void readOnly()
+    {
+        QxtPipe p1;
+        QxtPipe p2;
+        p1.connect(&p2,QIODevice::ReadOnly);
+
+        p1.write("hi");
+        QVERIFY(p1.bytesAvailable()==0);
+        QVERIFY(p2.bytesAvailable()==0);
+
+        p2.write("rehi");
+        QVERIFY(p1.readAll()=="rehi");
+        QVERIFY(p2.bytesAvailable()==0);
+    }
+    void writeOnly()
+    {
+        QxtPipe p1;
+        QxtPipe p2;
+        p1.connect(&p2,QIODevice::WriteOnly);
+
+        p1.write("hi");
+        QVERIFY(p1.bytesAvailable()==0);
+        QVERIFY(p2.readAll()=="hi");
+
+        p2.write("rehi");
+        QVERIFY(p1.bytesAvailable()==0);
+        QVERIFY(p2.bytesAvailable()==0);
     }
 };
 
