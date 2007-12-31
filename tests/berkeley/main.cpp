afc2d9a663ccd750b0936cba84ace3b1a0aec535
aep <none@none>
2007-12-31 15:20:22 +0000

    QxtBdbHashIterator
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401037

--- tests/berkeley/main.cpp
+++ tests/berkeley/main.cpp
@@ -9,37 +9,77 @@ Q_OBJECT
 private slots:
     void begin()
     {
-        db = new QxtBdbHash<qreal,QStringList>("test.db");
+        db.open("test.db");
+    }
+    void readInvalidIterator()
+    {
+        QVERIFY(db.begin().value().size()==0);
     }
     void insert()
     {
-        QVERIFY(db->insert(454.332,QStringList()<<"hello"<<"how are you?"));
-        QVERIFY(db->insert(235,QStringList()<<"hey"<<"how's it going?"));
-        QVERIFY(db->insert(234,QStringList()<<"jo"<<"sup?"));
+        QVERIFY(db.insert(454.332,QStringList()<<"hello"<<"how are you?"));
+        QVERIFY(db.insert(235,QStringList()<<"hey"<<"how's it going?"));
+        QVERIFY(db.insert(234,QStringList()<<"jo"<<"sup?"));
     }
     void contains()
     {
-        QVERIFY(db->contains(235));
-        QVERIFY(!db->contains(34.24));
+        QVERIFY(db.contains(235));
+        QVERIFY(!db.contains(34.24));
+    }
+    void implicitCopy()
+    {
+        QxtBdbHash<qreal,QStringList>  d=db;
+        QVERIFY(d.contains(235));
+        QVERIFY(!d.contains(34.24));
     }
     void read()
     {
-        QxtBdbHash<qreal,QStringList>  d=*db;
-        QVERIFY(d[235].at(1)=="how's it going?");
+        QVERIFY(db[235].at(1)=="how's it going?");
     }
+    void readBeginIterator()
+    {
+        QVERIFY(db.begin().value().at(1)=="sup?");
+    }
+    void readNextAndPrevIterator()
+    {
+        QxtBdbHashIterator<qreal,QStringList> it=db.begin();
+        ++it;
+
+        QVERIFY(it.value().at(1)=="how's it going?");
+        QVERIFY(it.key()==235);
+
+        --it;
+        QVERIFY(it.value().at(1)=="sup?");
+    }
+
+    void readIteratorOutOfBounds()
+    {
+        QxtBdbHashIterator<qreal,QStringList> it=db.begin();
+        --it;
+        it--;
+        --it;
+        it--;
+
+        QVERIFY(!it.isValid());
+        QVERIFY(it.value().count()==0);
+        QVERIFY(it.key()==0);
+
+        it+=4;
+    }
+
+
     void clear()
     {
-        QVERIFY(db->contains(235));
-        db->clear();
-        QVERIFY(!db->contains(235));
+        QVERIFY(db.contains(235));
+        db.clear();
+        QVERIFY(!db.contains(235));
     }
     void end()
     {
-        delete db;
     }
 
 private:
-    QxtBdbHash<qreal,QStringList> * db;
+    QxtBdbHash<qreal,QStringList> db;
 };
 
 QTEST_MAIN(Test)
