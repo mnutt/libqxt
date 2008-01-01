45ed5793720f7009e8f11580951f3f405b49d53e
aep <none@none>
2008-01-01 18:22:54 +0000

    forgot to delete those in-header-docs
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401054

--- src/sql/qxtsqlpackage.h
+++ src/sql/qxtsqlpackage.h
@@ -39,64 +39,17 @@ public:
     QxtSqlPackage(QObject *parent = 0);
     QxtSqlPackage(const QxtSqlPackage & other,QObject *parent = 0);
 
-    ///determinates if the package curently points to a valid row
     bool isValid();
-
-    ///curent pointer position
     int at();
-
-    /** \brief point to next entry
-
-    returns false if there is no next entry.\n
-    provided for easy porting from QSqlQuery.
-
-    \code	
-    while (query.next())
-    	{
-    	}
-    \endcode 
-    */
     bool next();
-
-    ///point to last entry in storage
     bool last();
-
-    ///point to first entry in storage
     bool first();
-
-    /** \brief return a cloumn in curent row
-
-    in contrast to QSqlQuery you have to provide the name of the key.
-
-    the entry is returned as QString becouse in most cases you need QString anyway, and converting to needed data type is easy.
-    \code
-    QString name = query.value("name");	
-    \endcode 
-    */
     QString value(const QString& key);
-
-    /** \brief read from QSqlQuery
-
-    read out a QSqlQuery and store the result. you may close the query after reading, the data will stay.
-
-    \code
-    QxSqlPackage::insert(QSqlQuery::exec("select name,foo,bar from table;"));
-    \endcode 
-    */
     void insert(QSqlQuery query);
-
-    ///Returns the number of rows stored
     int count() const;
-
-    ///serialise Data
     QByteArray data() const;
-
-    ///deserialise data
     void setData(const QByteArray& data);
-
-    ///return a specific row as Hash
     QHash<QString,QString> hash(int index);
-    ///return the curent row as Hash
     QHash<QString,QString> hash();
     QxtSqlPackage& operator= (const QxtSqlPackage& other);
 
