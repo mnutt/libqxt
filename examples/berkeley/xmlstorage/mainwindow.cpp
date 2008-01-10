ec16de1eb0ef3abf1e1d3eb7746831b1dddcff1e
aep <none@none>
2008-01-10 17:49:12 +0000

    trying to map to a view
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401189

--- examples/berkeley/xmlstorage/mainwindow.cpp
+++ examples/berkeley/xmlstorage/mainwindow.cpp
@@ -2,98 +2,21 @@
 #include <QTreeWidgetItem>
 #include <QDebug>
 #include <QFileDialog>
-#include "xml2bdb.h"
 
 MainWindow::MainWindow()
 {
     ui.setupUi(this);
-    tree.open("xml.db");
-    initTree();
-}
-
-void MainWindow::initTree()
-{
-    ui.treeWidget->clear();
-    QxtBdbTreeIterator<XmlNode> it=tree.root().child();
-    readTree(it,0);
-}
-
-void MainWindow::readTree(QxtBdbTreeIterator<XmlNode> it,QTreeWidgetItem * itema)
-{
-    while(it.isValid())
-    {
-        XmlNode c=it.value();
-        QTreeWidgetItem *item;
-        if(itema)
-            item=new QTreeWidgetItem(itema);
-        else
-            item=new QTreeWidgetItem(ui.treeWidget);
-
-
-        item->setText(0,c.name);
-        item->setText(2,c.value);
-
-        switch(c.type)
-        {
-            case QXmlStreamReader::NoToken:
-                item->setText(1,"NoToken");
-                break;
-            case QXmlStreamReader::Invalid:
-                item->setText(1,"Invalid");
-                break;
-            case QXmlStreamReader::StartDocument:
-                item->setText(1,"StartDocument");
-                break;
-            case QXmlStreamReader::EndDocument:
-                item->setText(1,"EndDocument");
-                break;
-            case QXmlStreamReader::StartElement:
-                item->setText(1,"StartElement");
-                break;
-            case QXmlStreamReader::EndElement:
-                item->setText(1,"EndElement");
-                break;
-            case QXmlStreamReader::Characters:
-                item->setText(1,"Characters");
-                break;
-            case QXmlStreamReader::Comment:
-                item->setText(1,"Comment");
-                break;
-            case QXmlStreamReader::DTD:
-                item->setText(1,"DTD");
-                break;
-            case QXmlStreamReader::EntityReference:
-                item->setText(1,"EntityReference");
-                break;
-            case QXmlStreamReader::ProcessingInstruction:
-                item->setText(1,"ProcessingInstruction");
-                break;
-        };
-
-
-        
-
-        QxtBdbTreeIterator<XmlNode> it2=it.child();
-        if(it2.isValid())
-            readTree(it2,item);
-
-        ++it;
-    }
+    model.open("xml.db");
+    ui.treeView->setModel(&model);
 }
 
 void MainWindow::on_actionLoad_triggered()
 {
-    QString f=QFileDialog::getOpenFileName ( this, "Load Xml file",QString(), "Xml File (*.xml)" );
+    QString f=QFileDialog::getOpenFileName (this, "Load Xml file",QString(), "Xml File (*.xml)");
     QFile file(f);
     file.open(QIODevice::ReadOnly);
-    Xml2Bdb x;
-    x.setDevice(&file);
-    tree.clear();
-    x.read(tree.root());
-    tree.dumpTree();
-    initTree();
+    model.loadXml(&file);
 }
-
 
 void MainWindow::on_actionQuit_triggered()
 {
--- examples/berkeley/xmlstorage/xmlstorage.pro
+++ examples/berkeley/xmlstorage/xmlstorage.pro
@@ -7,7 +7,7 @@ CONFIG+=qxt
 QXT+=berkeley
 
 
-HEADERS += node.h mainwindow.h xml2bdb.h
+HEADERS += node.h mainwindow.h xml2bdb.h model.h
 FORMS += mainwindow.ui
-SOURCES += node.cpp main.cpp mainwindow.cpp xml2bdb.cpp
+SOURCES += node.cpp main.cpp mainwindow.cpp xml2bdb.cpp model.cpp
 
