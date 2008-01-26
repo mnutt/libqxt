49359dd9252cb14485aa81a99796f5180b24206d
aep <none@none>
2008-01-26 16:57:59 +0000

    4.4 required
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401254

--- config.tests/db/main.cpp
+++ config.tests/db/main.cpp
@@ -1,9 +1,19 @@
 #include <db.h>
+
+#if DB_VERSION_MAJOR < 4
+#error db>=4.4 required
+#endif
+
+#if DB_VERSION_MINOR < 4
+#error db>=4.4 required
+#endif
+
+
 int main (int,char**)
 {
-	DB * db;
-        db_create(&db, NULL, 0);
-	return 0;
+    DB * db;
+    db_create(&db, NULL, 0);
+    return 0;
 }
 
 
