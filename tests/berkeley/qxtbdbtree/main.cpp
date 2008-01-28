62eed0fc19a2c6c61f155c633ff8507ca0c80725
aep <none@none>
2008-01-28 13:25:56 +0000

    stuff. (svn broke)
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401258

---
+++ tests/berkeley/qxtbdbtree/main.cpp
@@ -0,0 +1,50 @@
+#include <QxtBdbTree>
+#include <QTest>
+#include <QDebug>
+#include <QStringList>
+
+class Test: public QObject
+{
+Q_OBJECT 
+private slots:
+    void begin()
+    {
+        db.open("test.db");
+        db.clear();
+    }
+    void readRoot()
+    {
+        QVERIFY(db.root().value().size()==0);
+    }
+    void readInvalid()
+    {
+        QVERIFY(db.root().child().value().size()==0);
+        QVERIFY(db.root().next().value().size()==0);
+        QVERIFY(db.root().previous().value().size()==0);
+        QVERIFY(db.root().parent().value().size()==0);
+    }
+    void append()
+    {
+        QVERIFY(db.root().append(QStringList()<<"asda"<<"asfg").isValid());
+        QVERIFY(db.root().append(QStringList()<<"sh00"<<"xcv").append(QStringList()<<"vvvv").isValid());
+    }
+    void prepend()
+    {
+        QVERIFY(db.root().append(QStringList()<<"xylophon").prepend(QStringList()<<"you"<<"rock").isValid());
+    }
+
+    void read()
+    {
+        QVERIFY( (db.root().child()+1).child().value().at(0)=="vvvv");
+        QVERIFY( (db.root().child()+2).value().at(1)=="rock");
+    }
+    void end()
+    {
+    }
+
+private:
+    QxtBdbTree<QStringList> db;
+};
+
+QTEST_MAIN(Test)
+#include "main.moc"
