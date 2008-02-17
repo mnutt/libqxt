d9b504f1fab823f5b2a94302ed044e52f8f76439
zbenjamin <devnull@localhost>
2008-02-17 16:29:10 +0000

    added QxtScheduleView to trunk .. finally

---
+++ src/gui/qxtscheduleitemdelegate.h
@@ -0,0 +1,64 @@
+/****************************************************************************
+**
+** Copyright (C) Qxt Foundation. Some rights reserved.
+**
+** This file is part of the QxtGui module of the Qt eXTension library
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of th Common Public License, version 1.0, as published by
+** IBM.
+** 
+** This file is provided "AS IS", without WARRANTIES OR CONDITIONS OF ANY
+** KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY
+** WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR
+** FITNESS FOR A PARTICULAR PURPOSE.
+**
+** You should have received a copy of the CPL along with this file.
+** See the LICENSE file and the cpl1.0.txt file included with the source
+** distribution for more information. If you did not receive a copy of the
+** license, contact the Qxt Foundation.
+**
+** <http://libqxt.sourceforge.net>  <libqxt@gmail.com>
+**
+****************************************************************************/
+#ifndef SPLITITEMDELEGATE_H
+#define SPLITITEMDELEGATE_H
+
+#include <QAbstractItemDelegate>
+#include <QPainterPath>
+#include "qxtstyleoptionscheduleviewitem.h"
+#include "qxtglobal.h"
+
+class QPainter;
+
+/**
+    @author Benjamin Zeller <zbenjamin@users.sourceforge.net>
+*/
+class QXT_GUI_EXPORT QxtScheduleItemDelegate : public QAbstractItemDelegate
+{
+    Q_OBJECT
+    public:
+        
+        enum ItemPart
+        {
+            Top,
+            Middle,
+            Bottom,
+            Single
+        
+        };
+        
+        QxtScheduleItemDelegate                 ( QObject *parent = 0 );
+        ~QxtScheduleItemDelegate                ( );
+        
+        virtual void        paint               ( QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index ) const;
+        virtual void        paintItemBody       ( QPainter *painter, const QRect rect , const QxtStyleOptionScheduleViewItem & option ,const ItemPart part, const QModelIndex & index ) const;
+        virtual void        paintItemHeader     ( QPainter *painter, const QRect rect , const QxtStyleOptionScheduleViewItem & option , const QModelIndex & index ) const;
+        virtual void        paintSubItem        ( QPainter *painter, const QRect rect , const QxtStyleOptionScheduleViewItem & option , const QModelIndex & index ) const;
+        virtual QSize       sizeHint            ( const QStyleOptionViewItem & option , const QModelIndex & index ) const;
+        virtual void        createPainterPath   ( QPainterPath &emptyPath,const QRect &fullItemRect ,const int iRoundTop, const int iRoundBottom ) const;
+
+
+};
+
+#endif
---
+++ src/gui/qxtscheduleviewheadermodel_p.h
@@ -0,0 +1,80 @@
+/****************************************************************************
+**
+** Copyright (C) Qxt Foundation. Some rights reserved.
+**
+** This file is part of the QxtGui module of the Qt eXTension library
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of th Common Public License, version 1.0, as published by
+** IBM.
+** 
+** This file is provided "AS IS", without WARRANTIES OR CONDITIONS OF ANY
+** KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY
+** WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR
+** FITNESS FOR A PARTICULAR PURPOSE.
+**
+** You should have received a copy of the CPL along with this file.
+** See the LICENSE file and the cpl1.0.txt file included with the source
+** distribution for more information. If you did not receive a copy of the
+** license, contact the Qxt Foundation.
+**
+** <http://libqxt.sourceforge.net>  <libqxt@gmail.com>
+**
+****************************************************************************/
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qxt API.  It exists for the convenience
+// of other Qxt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+
+#ifndef SPLITVIEWHEADERMODEL_H
+#define SPLITVIEWHEADERMODEL_H
+
+#include <QAbstractTableModel>
+#include <QtGlobal>
+#include <QPointer>
+
+/**
+ *@author Benjamin Zeller <zbenjamin@users.sourceforge.net>
+ *@desc This Model is used to tell the Header how many rows and columns the view has 
+ */
+
+class QxtScheduleView;
+
+class QxtScheduleViewHeaderModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    
+    public:
+        QxtScheduleViewHeaderModel( QObject *parent = 0 );
+        
+        void                    setDataSource   ( QxtScheduleView *dataSource );
+        
+        virtual QModelIndex     parent          ( const QModelIndex & index                                      ) const;
+        virtual int             rowCount        ( const QModelIndex & parent = QModelIndex()                     ) const;
+        virtual int             columnCount     ( const QModelIndex & parent = QModelIndex()                     ) const;
+        virtual  QVariant       data            ( const QModelIndex & index, int role = Qt::DisplayRole          ) const;
+        virtual bool            setData         ( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole );
+        virtual bool            insertRow       ( int row, const QModelIndex & parent = QModelIndex() );
+        virtual QVariant        headerData      ( int section, Qt::Orientation orientation,  int role = Qt::DisplayRole ) const;
+        virtual Qt::ItemFlags   flags           ( const QModelIndex &index) const;
+        virtual bool            hasChildren     ( const QModelIndex & parent = QModelIndex() ) const;   
+        
+    public slots:
+        void                    newZoomDepth    ( const int zoomDepth );
+        void                    viewModeChanged ( const int viewMode  );
+    
+    private:
+        QPointer<QxtScheduleView> m_dataSource;
+        int                       m_rowCountBuffer;
+        int                       m_colCountBuffer;
+};
+
+#endif
