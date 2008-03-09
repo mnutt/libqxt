3a57d477007f2a83bd7a95a7a5b9d4fa24a0dcc7
ahigerd <none@none>
2008-03-09 04:01:44 +0000

    docs for QxtCommandOptions
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401383

--- src/core/qxtcommandoptions.h
+++ src/core/qxtcommandoptions.h
@@ -35,14 +35,114 @@ class QxtCommandOptionsPrivate;
 class QTextStream;
 class QIODevice;
 
-struct QXT_CORE_EXPORT QxtCommandOptions
+/**
+\class QxtCommandOptions QxtCommandOptions
+
+\ingroup QxtCore
+
+\brief Parser for command-line options
+
+This class is used by applications that need to accept command-line arguments.  It can
+also automatically generate help text, which keeps it from accidentally falling out of
+sync with the supported options, and it can produce warnings for common errors.
+
+It accepts Windows-style ("/option"), UNIX-style ("-option"), and GNU-style
+("--option") options. By default, QxtCommandOptions uses Windows-style options
+(QxtCommandOptions::Slash) on Windows and GNU-style options (QxtCommandOptions::DoubleDash)
+on UNIX and Mac. When using GNU-style options, single-character option names only
+require a single leading dash and can be grouped together, for example, "-abc".
+
+Any parameter that does not start with the option prefix is considered a positional
+parameter. Most applications treat positional parameters as filenames. When using
+GNU- or UNIX-style options, use a double dash ("--") alone to force the remaining
+parameters to be interpreted as positional parameters.
+
+To use QxtCommandOptions, first add the supported options using the add() and alias()
+methods, then process the command line using the parse() method, and finally retrieve
+the processed options using the positional(), count(), value() and/or parameters()
+methods.
+
+Mutually-exclusive options can be specified by using the \a group parameter to add().
+Only one option in a group will be accepted on the command line; if multiple options
+are provided, only the last one takes effect.
+
+Some options may accept an optional or required parameter. Depending on the value
+of the paramStyle() property, the parameter may be separated from the option by
+an equals sign ("/option=value") or by a space ("-option value"). By default,
+Windows uses an equals sign and UNIX and Mac accept both an equals sign and a
+space. Optional parameters always require an equals sign. Note that, when using
+GNU-style options, single-character options cannot have optional parameters.
+
+A simple archiving application might use code similar to this:
+\code
+QxtCommandOptions options;
+options.add("compress", "create a new archive");
+options.alias("compress", "c");
+options.add("extract", "extract files from an archive");
+options.alias("extract", "x");
+options.add("level", "set the compression level (0-9)", QxtCommandOptions::Required);
+options.alias("level", "l");
+options.add("verbose", "show more information about the process; specify twice for more detail", QxtCommandOptions::AllowMultiple);
+options.alias("verbose", "v");
+options.add("help", "show this help text");
+options.alias("help", "h");
+options.parse(QCoreApplication::arguments());
+if(options.count("help") || options.showUnrecognizedWarning()) {
+    options.showUsage();
+    return -1;
+}
+bool verbose = options.count("verbose");
+int level = 5;
+if(options.count("level")) {
+    level = options.value("level").toInt();
+}
+\endcode
+
+\sa QCoreApplication::arguments()
+*/
+class QXT_CORE_EXPORT QxtCommandOptions
 {
 Q_DECLARE_TR_FUNCTIONS(QxtCommandOptions)
 
 public:
-    enum FlagStyle { DoubleDash, SingleDash, Slash };
-    enum ParamStyle { Space = 1, Equals = 2, SpaceAndEquals = 3 };
-    enum ParamType { NoValue = 0, Optional = 1, Required = 2, AllowMultiple = 4, Undocumented = 8 };
+    /**
+     * \enum QxtCommandOptions::FlagStyle
+     * This enum type defines which type of option prefix is used.
+     * Slash is the default on Windows.
+     * DoubleDash is the default on all other platforms.
+     */
+    enum FlagStyle {
+        DoubleDash,         /*!< Two dashes (GNU-style) */
+        SingleDash,         /*!< One dash (UNIX-style) */
+        Slash               /*!< Forward slash (Windows-style) */
+    };
+    /**
+     * \enum QxtCommandOptions::ParamStyle
+     * This enum type defines what syntax is used for options that
+     * require parameters. Equals is the default on Windows.
+     * SpaceAndEquals is the default on all other platforms.
+     */
+    enum ParamStyle {
+        Space = 1,          /*!< Space ("-option value") */
+        Equals = 2,         /*!< Equals sign ("/option=value") */
+        SpaceAndEquals = 3  /*!< Accept either */
+    };
+    /**
+     * \enum QxtCommandOptions::ParamType
+     * \flags QxtCommandOptions::ParamTypes
+     * This enum type is used to specify flags that control the
+     * interpretation of an option.
+     *
+     * The ParamTypes type is a typedef for QFlags<ParamType>. It stores
+     * an OR combination of ParamType values.
+     */
+    enum ParamType {
+        NoValue = 0,        /*!< The option does not accept a value. */
+        Optional = 1,       /*!< The option may accept a value. */
+        Required = 2,       /*!< The option requires a value. */
+        AllowMultiple = 4,  /*!< The option may be passed multiple times. */
+        Undocumented = 8    /*!< The option is not output in the help text. */
+    };
     Q_DECLARE_FLAGS(ParamTypes, ParamType)
 
     QxtCommandOptions();
@@ -64,6 +164,7 @@ public:
     QVariant value(const QString& name) const;
     QMultiHash<QString, QVariant> parameters() const;
 
+    void parse(int argc, char** argv);
     void parse(QStringList params);
     
     void showUsage(bool showQtOptions = false, QIODevice* device = 0) const;
