ea860b634cb7a6a8f8932a80dcf4cd5f0351ac48
ahigerd <devnull@localhost>
2008-05-20 04:34:09 +0000

    show server numerics, support ctrl+tab, add part and names commands, add unread notification

--- examples/ircpeer/mainwindow.cpp
+++ examples/ircpeer/mainwindow.cpp
@@ -1,4 +1,6 @@
 #include "mainwindow.h"
+#include <QApplication>
+#include <QStyle>
 
 #include <QtGui>
 MainWindow::MainWindow()
@@ -6,6 +8,7 @@ MainWindow::MainWindow()
     centralwidget = new QWidget(this);
     gridLayout = new QGridLayout(centralwidget);
     tabWidget = new QTabWidget(centralwidget);
+    tabWidget->setTabPosition(QTabWidget::South);
     gridLayout->addWidget(tabWidget, 0, 0, 1, 2);
     pushButton = new QPushButton(centralwidget);
     gridLayout->addWidget(pushButton, 1, 1, 1, 1);
@@ -19,18 +22,19 @@ MainWindow::MainWindow()
 
     QObject::connect(lineEdit, SIGNAL(returnPressed()), this, SLOT(send()));
     QObject::connect(pushButton, SIGNAL(clicked()), this, SLOT(send()));
+    QObject::connect(tabWidget, SIGNAL(currentChanged(int)), this, SLOT(tabActivated(int)));
+    lineEdit->installEventFilter(this);
 
     irc.attachSlot("PRIVMSG",this, SLOT(receiveMessage(IRCName, QByteArray, QByteArray)));
+    irc.attachSlot("system",this, SLOT(receiveMessage(IRCName, QByteArray, QByteArray)));
+    irc.attachSlot("numeric",this, SLOT(receiveMessage(IRCName, QByteArray, QByteArray)));
     irc.attachSignal(this, SIGNAL(sendMessage(IRCName, QByteArray, QByteArray)), "PRIVMSG");
     QObject::connect(&irc, SIGNAL(peerConnected()), this, SLOT(peerConnected()));
     QObject::connect(&irc, SIGNAL(peerError( QAbstractSocket::SocketError)), this, SLOT(peerError( QAbstractSocket::SocketError)));
 
-
     connect(this, SIGNAL(sendMessage(IRCName, QByteArray, QByteArray)), this, SLOT(receiveMessage(IRCName, QByteArray, QByteArray)));
 
-
     setWindowTitle("Qxt RPC IRC example");
-
 
     pushButton->setText("send");
 
@@ -71,6 +75,10 @@ void MainWindow::peerError ( QAbstractSocket::SocketError e)
 
 void MainWindow::receiveMessage(IRCName nick, QByteArray channel, QByteArray message)
 {
+    if(channel == "")
+        channel = condiag.hostname();
+    else if(channel == condiag.nickname())
+        channel = nick.nick;
     if(!channels.contains(channel))
     {
         channels[channel] = new QTextBrowser();
@@ -80,6 +88,9 @@ void MainWindow::receiveMessage(IRCName nick, QByteArray channel, QByteArray mes
     channels[channel]->append(QString::fromUtf8(QByteArray("<") + nick.nick + "> " + message));
     channels[channel]->moveCursor(QTextCursor::End);
     channels[channel]->ensureCursorVisible();
+    int index = tabWidget->indexOf(channels[channel]);
+    if(index != tabWidget->currentIndex())
+        tabWidget->setTabIcon(index, qApp->style()->standardIcon(QStyle::SP_ArrowRight));
 }
 
 
@@ -96,6 +107,19 @@ void MainWindow::send()
             receiveMessage(IRCName(condiag.hostname()),msg.mid(6),"you have joined "+msg.mid(6));        
             tabWidget->setCurrentWidget(channels[msg.mid(6)]);
         }
+        else if(msg.startsWith("/names"))
+        {
+            if(msg == "/names")
+                irc.call("NAMES", QVariant(), tabWidget->tabText(tabWidget->currentIndex()).toUtf8());
+            else
+                irc.call("NAMES", QVariant(), msg.mid(7));
+        }
+        else if(msg == "/part")
+        {
+            irc.call("PART", QVariant(), tabWidget->tabText(tabWidget->currentIndex()).toUtf8());
+            tabWidget->currentWidget()->deleteLater();
+            tabWidget->removeTab(tabWidget->currentIndex());
+        }
         else
         {
             receiveMessage(IRCName(condiag.hostname()),condiag.hostname(),"unknown command "+msg);
@@ -110,5 +134,23 @@ void MainWindow::send()
     lineEdit->clear();
 }
 
+bool MainWindow::eventFilter(QObject* obj, QEvent* event) {
+    if(obj != lineEdit) return false;
+    if(event->type() != QEvent::KeyPress) return false;
+    QKeyEvent* ke = static_cast<QKeyEvent*>(event);
+    if(ke->key() != Qt::Key_Tab && ke->key() != Qt::Key_Backtab) return false;
+    if(!ke->modifiers().testFlag(Qt::ControlModifier)) return false;
+    int nextTab = tabWidget->currentIndex();
+    if(ke->key() == Qt::Key_Backtab)
+        nextTab--;
+    else
+        nextTab++;
+    if(nextTab == -1) nextTab = tabWidget->count() - 1;
+    if(nextTab == tabWidget->count()) nextTab = 0;
+    tabWidget->setCurrentIndex(nextTab);
+    return true;
+}
 
-
+void MainWindow::tabActivated(int index) {
+    tabWidget->setTabIcon(index, QIcon());
+}
