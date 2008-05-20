f3031aa4a0eb21307be61a346e96852f6cd130c7
ahigerd <none@none>
2008-05-20 06:52:35 +0000

    remember last connection settings
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401536

--- examples/ircpeer/dialog.h
+++ examples/ircpeer/dialog.h
@@ -3,11 +3,15 @@
 
 class ConnectDialog : public QDialog
 {
+Q_OBJECT
 public:
     ConnectDialog();
     QByteArray hostname();
     int port();
     QString nickname();
+
+private slots:
+    void save();
 
 private:
     Ui::Dialog ui;
