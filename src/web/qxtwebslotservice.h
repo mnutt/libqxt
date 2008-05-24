24607fe06996903b447ca8c49963aa3bf26ce845
aep <devnull@localhost>
2008-05-24 20:12:51 +0000

    QxtWebSlotService

---
+++ src/web/qxtwebslotservice.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+**
+** Copyright (C) Qxt Foundation. Some rights reserved.
+**
+** This file is part of the QxtWeb  module of the Qt eXTension library
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of th Common Public License, version 1.0, as published by
+** IBM.
+**
+** This file is provided "AS IS", without WARRANTIES OR CONDITIONS OF ANY
+** KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY
+** WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR
+** FITNESS FOR A PARTICULAR PURPOSE.
+**
+** You should have received a copy of the CPL along with this file.
+** See the LICENSE file and the cpl1.0.txt file included with the source
+** distribution for more information. If you did not receive a copy of the
+** license, contact the Qxt Foundation.
+**
+** <http://www.libqxt.org>  <foundation@libqxt.org>
+**
+****************************************************************************/
+
+#ifndef QXTWEBSLOTSERVICE_H_ijakbsd
+#define QXTWEBSLOTSERVICE_H_ijakbsd
+
+#include "qxtabstractwebservice.h"
+#include <QUrl>
+
+
+class QxtSlotService : public QxtAbstractWebService 
+{
+Q_OBJECT
+public:
+    QxtSlotService(QxtAbstractWebSessionManager* sm, QObject* parent = 0);
+protected:
+
+    QUrl self(QxtWebRequestEvent* event);
+
+    virtual void pageRequestedEvent(QxtWebRequestEvent* event);
+    virtual void functionInvokedEvent(QxtWebRequestEvent* event);
+};
+
+
+
+#endif
+
