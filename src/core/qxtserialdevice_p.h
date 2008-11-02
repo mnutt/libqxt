8b60c826e1fe10ad3909baf586ba3ff34091e900
ahigerd <devnull@localhost>
2008-11-02 04:13:58 +0000

    introducing QxtSerialDevice; still needs convenience headers, Windows implementation, and some docs

---
+++ src/core/qxtserialdevice_p.h
@@ -0,0 +1,37 @@
+#ifndef QXTSERIALDEVICE_P_H
+#define QXTSERIALDEVICE_P_H
+
+#include "qxtserialdevice.h"
+#include <QByteArray>
+
+#ifdef Q_OS_UNIX
+#include <termios.h>
+#endif
+
+class QSocketNotifier;
+
+class QxtSerialDevicePrivate : public QObject, public QxtPrivate<QxtSerialDevice> {
+Q_OBJECT
+public:
+    QxtSerialDevicePrivate();
+    
+    int fd;
+    bool errorState;
+    QSocketNotifier* notifier;
+    mutable QByteArray buffer;
+    QString device;
+    int baud, flow, format;
+    QxtSerialDevice::PortSettings portSettings; // duplicated for convenience
+#ifdef Q_OS_UNIX
+    termios reset, settings;
+#endif
+
+    qint64 deviceBuffer() const;
+    bool setPortSettings(QxtSerialDevice::PortSettings settings);
+    
+public slots:
+    int fillBuffer();
+    int constFillBuffer() const;
+};
+
+#endif // QXTSERIALDEVICE_P_H
