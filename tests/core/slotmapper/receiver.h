a24653a8b6d56023b62e3eca211ee1a9f96dc359
jpnurmi <none@none>
2009-01-22 19:48:08 +0000

    Added QxtSlotMapper unit tests.
    --HG--
    extra : convert_revision : svn%3A16f47a83-c923-0410-902c-c9163ce98875/trunk%401818

---
+++ tests/core/slotmapper/receiver.h
@@ -0,0 +1,77 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+#include <QtCore>
+
+class Receiver : public QObject
+{
+    Q_OBJECT
+public:
+    Receiver() { }
+
+public slots:
+    void intSlot(int val) { ints << val; }
+    void uintSlot(uint val) { uints << val; }
+    void qlonglongSlot(qlonglong val) { qlonglongs << val; }
+    void qulonglongSlot(qulonglong val) { qulonglongs << val; }
+    void boolSlot(bool val) { bools << val; }
+    void doubleSlot(double val) { doubles << val; }
+    //void charSlot(const char* val) { chars << val; } "const char*" is not a meta type
+    void byteArraySlot(const QByteArray& val) { byteArrays << val; }
+    void bitArraySlot(const QBitArray& val) { bitArrays << val; }
+    void stringSlot(const QString& val) { strings << val; }
+    //void latin1StringList(const QLatin1String& val) { latin1Strings << val; } "QLatin1String" is not a meta type
+    void stringListSlot(const QStringList& val) { stringLists << val; }
+    void qcharSlot(const QChar& val) { qchars << val; }
+    void dateSlot(const QDate& val) { dates << val; }
+    void timeSlot(const QTime& val) { times << val; }
+    void dateTimeSlot(const QDateTime& val) { dateTimes << val; }
+    void listSlot(const QVariantList& val) { lists << val; }
+    void mapSlot(const QVariantMap& val) { maps << val; }
+    void sizeSlot(const QSize& val) { sizes << val; }
+    void sizeFSlot(const QSizeF& val) { sizeFs << val; }
+    void pointSlot(const QPoint& val) { points << val; }
+    void pointFSlot(const QPointF& val) { pointFs << val; }
+    void lineSlot(const QLine& val) { lines << val; }
+    void lineFSlot(const QLineF& val) { lineFs << val; }
+    void rectSlot(const QRect& val) { rects << val; }
+    void rectFSlot(const QRectF& val) { rectFs << val; }
+    void urlSlot(const QUrl& val) { urls << val; }
+    void localeSlot(const QLocale& val) { locales << val; }
+    void regExpSlot(const QRegExp& val) { regExps << val; }
+    void globalColorSlot(Qt::GlobalColor val) { globalColors << val; }
+
+public:
+    QList<int> ints;
+    QList<uint> uints;
+    QList<qlonglong> qlonglongs;
+    QList<qulonglong> qulonglongs;
+    QList<bool> bools;
+    QList<double> doubles;
+    //QList<const char*> chars; "const char*" is not a meta type
+    QList<QByteArray> byteArrays;
+    QList<QBitArray> bitArrays;
+    QList<QString> strings;
+    //QList<QLatin1String> latin1Strings; "QLatin1String" is not a meta type
+    QList<QStringList> stringLists;
+    QList<QChar> qchars;
+    QList<QDate> dates;
+    QList<QTime> times;
+    QList<QDateTime> dateTimes;
+    QList<QVariantList> lists;
+    QList<QVariantMap> maps;
+    QList<QSize> sizes;
+    QList<QSizeF> sizeFs;
+    QList<QPoint> points;
+    QList<QPointF> pointFs;
+    QList<QLine> lines;
+    QList<QLineF> lineFs;
+    QList<QRect> rects;
+    QList<QRectF> rectFs;
+    QList<QUrl> urls;
+    QList<QLocale> locales;
+    QList<QRegExp> regExps;
+    QList<Qt::GlobalColor> globalColors;
+};
+
+#endif // RECEIVER_H
---
+++ tests/core/slotmapper/sender.h
@@ -0,0 +1,48 @@
+#ifndef SENDER_H
+#define SENDER_H
+
+#include <QtCore>
+
+class Sender : public QObject
+{
+    Q_OBJECT
+public:
+    Sender() { }
+
+signals:
+    void intSignal(int val);
+    void uintSignal(uint val);
+    void qlonglongSignal(qlonglong val);
+    void qulonglongSignal(qulonglong val);
+    void boolSignal(bool val);
+    void doubleSignal(double val);
+    //void charSignal(const char* val); "const char*" is not a meta type
+    void byteArraySignal(const QByteArray& val);
+    void bitArraySignal(const QBitArray& val);
+    void stringSignal(const QString& val);
+    //void latin1StringSignal(const QLatin1String& val); "QLatin1String" is not a meta type
+    void stringListSignal(const QStringList& val);
+    void qcharSignal(const QChar& val);
+    void dateSignal(const QDate& val);
+    void timeSignal(const QTime& val);
+    void dateTimeSignal(const QDateTime& val);
+    void listSignal(const QVariantList& val);
+    void mapSignal(const QVariantMap& val);
+    void sizeSignal(const QSize& val);
+    void sizeFSignal(const QSizeF& val);
+    void pointSignal(const QPoint& val);
+    void pointFSignal(const QPointF& val);
+    void lineSignal(const QLine& val);
+    void lineFSignal(const QLineF& val);
+    void rectSignal(const QRect& val);
+    void rectFSignal(const QRectF& val);
+    void urlSignal(const QUrl& val);
+    void localeSignal(const QLocale& val);
+    void regExpSignal(const QRegExp& val);
+    void globalColorSignal(Qt::GlobalColor val);
+
+private:
+    friend class tst_QxtSlotMapper;
+};
+
+#endif // SENDER_H
