5cac24866ebf9c77695ee1576a9eb55644f7a305
jpnurmi <devnull@localhost>
2009-04-20 02:07:39 +0300

    Improved tests/other/no_keywords to check for enabled Qxt modules.

--- tests/other/no_keywords/main.cpp
+++ tests/other/no_keywords/main.cpp
@@ -1,11 +1,28 @@
+#include <QtGlobal>
+#if defined(Q_WS_WIN) && defined(QXT_BERKELEY_LIB)
 #include <QxtBerkeley>
+#endif
+#ifdef QXT_CORE_LIB
 #include <QxtCore>
+#endif
+#ifdef QXT_CRYPTO_LIB
 #include <QxtCrypto>
+#endif
+#ifdef QXT_GUI_LIB
 #include <QxtGui>
+#endif
+#ifdef QXT_NETWORK_LIB
 #include <QxtNetwork>
+#endif
+#ifdef QXT_SQL_LIB
 #include <QxtSql>
+#endif
+#ifdef QXT_WEB_LIB
 #include <QxtWeb>
+#endif
 
-int main()
+int main(int argc, char* argv[])
 {
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
 }
