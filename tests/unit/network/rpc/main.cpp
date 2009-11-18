c26b223fd4e40b6675e27d14fdf6cfaea45173b8
marcusl <macke@yar.nu>
2009-11-18 23:51:10 +0100

    rpcservive: delete socket later to avoid double delete (fixes #23) + add unit test

--- tests/unit/network/rpc/main.cpp
+++ tests/unit/network/rpc/main.cpp
@@ -3,6 +3,7 @@
 #include <qxtfifo.h>
 #include <QCoreApplication>
 #include <QTest>
+#include <QThread>
 #include <QSignalSpy>
 #include <QBuffer>
 #include <QDebug>
@@ -12,8 +13,12 @@
 class RPCTest: public QObject
 {
     Q_OBJECT
+public:
+    RPCTest() : client_id(0) {}
+
 private:
-    QxtRPCPeer* peer;
+    quint64 client_id;
+
 signals:
     void wave(QString);
     void counterwave(QString);
@@ -22,10 +27,23 @@ signals:
 
 private slots:
     void initTestCase()
-    {}
+    {
+        client_id = 0;
+    }
 
+    void clientConnected(quint64 id) 
+    {
+        client_id = id;
+    }
 
-
+    void serverError(QAbstractSocket::SocketError e)
+    {
+        if (e != QAbstractSocket::RemoteHostClosedError) {
+            qCritical("socket error: %i", e);
+            QVERIFY2(false, "socket error");
+        }
+    }
+        
     void loopback()
     {
         QxtRPCService peer(new QxtFifo, 0);
@@ -89,8 +107,13 @@ private slots:
 
 
         QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
         QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
         QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+
 
         QVERIFY2 (spy.count()> 0, "no signal received" );
         QVERIFY2 (spy.count()< 2, "wtf, two signals received?" );
@@ -98,6 +121,51 @@ private slots:
         QList<QVariant> arguments = spy.takeFirst();
         QVERIFY2(arguments.at(1).toString()=="world","argument missmatch");
     }
+
+    void TcpServerIoDisconnect()
+    {
+        // check that disconnect from server doesn't crash client (which it did in 0.5.1)
+
+        QxtRPCPeer server;
+        QVERIFY2(connect(&server, SIGNAL(clientConnected(quint64)),this,SLOT(clientConnected(quint64))),"cannot connect");
+        QVERIFY2(connect(&server, SIGNAL(serverError(QAbstractSocket::SocketError)),this,SLOT(serverError(QAbstractSocket::SocketError))),"cannot connect");
+        QVERIFY(server.listen (QHostAddress::LocalHost, 23444));
+
+        QVERIFY(client_id == 0);
+
+        QxtRPCPeer client;
+        client.connect (QHostAddress::LocalHost, 23444);
+        QVERIFY2(connect(&client, SIGNAL(serverError(QAbstractSocket::SocketError)),this,SLOT(serverError(QAbstractSocket::SocketError))),"cannot connect");
+        QVERIFY(qobject_cast<QTcpSocket*>(client.device())->waitForConnected ( 30000 ));
+
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+        QCoreApplication::processEvents ();
+        QCoreApplication::processEvents ();
+
+        QVERIFY(client.isClient());
+        QVERIFY(client_id != 0);
+  
+        server.disconnectClient(client_id);
+
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+        QThread::yieldCurrentThread();
+        QCoreApplication::processEvents ();
+
+
+        QVERIFY(server.isServer());
+        QVERIFY(!client.isClient());
+    }
+
     void cleanupTestCase()
     {}
 };
