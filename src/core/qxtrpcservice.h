1e77b67e884473830439e7afa9ce5bffd2f76727
ahigerd <none@none>
2010-04-01 10:20:43 -0500

    commenting up the black magic that is QxtRPCService

--- src/core/qxtrpcservice.h
+++ src/core/qxtrpcservice.h
@@ -40,7 +40,7 @@ class QxtAbstractSignalSerializer;
 class QxtRPCServicePrivate;
 class QXT_CORE_EXPORT QxtRPCService : public QObject
 {
-    Q_OBJECT
+Q_OBJECT
 public:
     QxtRPCService(QObject* parent = 0);
     explicit QxtRPCService(QIODevice* device, QObject* parent = 0);
@@ -61,7 +61,8 @@ public:
     void setConnectionManager(QxtAbstractConnectionManager* manager);
 
     bool attachSignal(QObject* sender, const char* signal, const QString& rpcFunction = QString());
-    bool attachSlot(const QString& rpcFunction, QObject* recv, const char* slot, Qt::ConnectionType type = Qt::AutoConnection);
+    bool attachSlot(const QString& rpcFunction, QObject* recv, const char* slot,
+            Qt::ConnectionType type = Qt::AutoConnection);
     void detachSignals(QObject* obj);
     void detachSlots(QObject* obj);
     void detachObject(QObject* obj);
@@ -71,22 +72,19 @@ public Q_SLOTS:
     void disconnectServer();
     void disconnectAll();
 
-    void call(QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(), const QVariant& p3 = QVariant(),
-              const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(), const QVariant& p6 = QVariant(),
-              const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
-    void call(QList<quint64> ids, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(), const QVariant& p3 = QVariant(),
-              const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(), const QVariant& p6 = QVariant(),
-              const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
-    void call(quint64 id, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(), const QVariant& p3 = QVariant(),
-              const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(), const QVariant& p6 = QVariant(),
-              const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
-    void callExcept(quint64 id, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(), const QVariant& p3 = QVariant(),
-                    const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(), const QVariant& p6 = QVariant(),
-                    const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
+    void call(QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(),
+              const QVariant& p3 = QVariant(), const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(),
+              const QVariant& p6 = QVariant(), const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
+    void call(QList<quint64> ids, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(),
+              const QVariant& p3 = QVariant(), const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(),
+              const QVariant& p6 = QVariant(), const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
+    void call(quint64 id, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(),
+              const QVariant& p3 = QVariant(), const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(),
+              const QVariant& p6 = QVariant(), const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
+    void callExcept(quint64 id, QString fn, const QVariant& p1 = QVariant(), const QVariant& p2 = QVariant(),
+                    const QVariant& p3 = QVariant(), const QVariant& p4 = QVariant(), const QVariant& p5 = QVariant(),
+                    const QVariant& p6 = QVariant(), const QVariant& p7 = QVariant(), const QVariant& p8 = QVariant());
 
-    /*!
-     * Detaches all signals and slots for the object that emitted the signal connected to detachSender().
-     */
     void detachSender();
 
 Q_SIGNALS:
